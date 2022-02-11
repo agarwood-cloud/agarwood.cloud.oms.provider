6c47f1bf1ec6ff9489c31bdb378163eb8dce7da0
agarwood <676786620@qq.com>
2022-02-11 15:28:36 +0800

    test:hello world

---
+++ src/test/java/com/agarwood/oms/OmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agarwood.oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
